79eca7f601be8fea6883f2c16af48c6386195680
Zain <zain.syed@vodafone.com>
2022-11-05 11:54:25 +0000

    updates

--- sw.js
+++ sw.js
@@ -1,9 +1,14 @@
 //Service Worker Version
-const version =  2
+const version =  1
 
 const staticCache = `staticCache_${version}`
 const dynamicCache = `dynamicCache${version}`
 
+
+
+//CORE assets that make up the WEB APPLICATION SHELL 
+// - index page, core css, core javascript files, logos etc
+// List of all the asset requests that we want to PRECACHE
 const staticAssets = [
     './',
     './index.html',
@@ -23,7 +28,7 @@ const staticAssets = [
 self.addEventListener('install', (event) => {
     
     console.log("Installation of Service Worker version:", version)
-    console.log(`Open cache ${staticCache} and store static assets`)
+    console.log(`Open cache ${staticCache} and store core static assets`)
 
     event.waitUntil(
         caches.open(staticCache)
@@ -40,12 +45,14 @@ self.addEventListener('activate', () => {
 //Listen for Fetch event
 self.addEventListener('fetch', (event) => {
     console.log("A fetch event occured: ", event.request.url)
+
+    //This allows us to in effect pause the fetch event and respond with ur own custom event
     event.respondWith(
         caches.match(event.request)
               .then(cacheResponse => {
                 //If it exists in cache then serve from cache - otherwise make a fetch request to the server
                  return cacheResponse || fetch(event.request).then(serverReponse => {
-                    //once we get the response from the server lets store it in the dynamic cache by cloning it
+                    //once we get the response stream from the server lets store it in the dynamic cache by cloning it
                     return caches.open(dynamicCache)
                           .then(cache => {
                             cache.put(event.request, serverReponse.clone())
