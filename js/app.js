2c4d0ce2f0dc9c7f7b14693d43b4a6dc5ca18ff0
Zain <zain.syed@vodafone.com>
2022-11-06 16:45:26 +0000

    Added prettier config

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 function init() {
-   if('serviceWorker' in navigator) {
-     //Lets Register our Service Worker sw.js
-     navigator.serviceWorker.register('./sw.js')
-              .then((registration) => console.log('A Service Worker has been registered'))
-              .catch(err => console.log("Error registering a service worker"))
-      
-      // 2. See if the page is currently has a service worker.
-      if (navigator.serviceWorker.controller) {
-        console.log('app.js: we have a service worker for this webpage');
-      }
+  if ('serviceWorker' in navigator) {
+    //Lets Register our Service Worker sw.js
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then((registrationObject) => console.log('A Service Worker has been registered ', registrationObject))
+      .catch((err) => console.log('Error registering a service worker ', err))
 
-      // 3. Register a handler to detect when a new or
-      navigator.serviceWorker.oncontrollerchange = () => {
-        console.log('app.js: New service worker activated');
-      };
-   } else {
-     console.log('Sorry this browser does not support Service Workers')
-   }
+    // 2. See if the page is currently has a service worker.
+    if (navigator.serviceWorker.controller) {
+      console.log('app.js: we have a service worker for this webpage')
+    }
+
+    // 3. Register a handler to detect when a new or
+    navigator.serviceWorker.oncontrollerchange = () => {
+      console.log('app.js: New service worker activated')
+    }
+  } else {
+    console.log('Sorry this browser does not support Service Workers')
+  }
 }
 
-
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener('DOMContentLoaded', init)
--- sw.js
+++ sw.js
@@ -3,20 +3,20 @@ const version = 1
 
 //Listen for Installation event of Service Worker
 self.addEventListener('install', () => {
-    console.log("Installation of Service Worker version:", version)
+  console.log('Installation of Service Worker version:', version)
 })
 
 //Listen for Activation event
 self.addEventListener('activate', () => {
-    console.log("Activation of Service Worker version:", version)
+  console.log('Activation of Service Worker version:', version)
 })
 
 //Listen for Fetch event
 self.addEventListener('fetch', (event) => {
-    console.log("A fetch event occured: ", event.request.url)
+  console.log('A fetch event occured: ', event.request.url)
 })
 
 //Listen for Background sync event
 self.addEventListener('sync', (event) => {
-    console.log(`Back Online: A background sync event happened for ${event.target}`)
+  console.log(`Back Online: A background sync event happened for ${event.target}`)
 })
