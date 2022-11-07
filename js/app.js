36e3e879f3f25a91d497d518051eda4b778b5cb6
Zain <zain.syed@vodafone.com>
2022-11-07 10:47:16 +0000

    updates

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
-const cacheName = 'myCache'
-
+//Create Web Worker
+const worker = new Worker('./js/webworker.js')
 
 function init() {
+
   //Get references to the buttons
   const changeBackgroundColourBtn = document.querySelector('#changeBackgroundColourBtn')
   const calculatePrimeNumbersBtn = document.querySelector('#calculatePrimeNumbersBtn')
   const results = document.querySelector('#results')
 
-  //1. Change background colour of page
+  //Change background colour of page
   changeBackgroundColourBtn.addEventListener('click', () => {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
@@ -16,36 +17,18 @@ function init() {
     document.body.style.background = bgColor;
     })
 
-  //2. Time consuming Javascipt to calculate Primes
-  // program to print prime numbers between the two numbers
-  //EXERCISE: Lets calculate the Prime numbers in a Web Worker on it's own thread so it's non blocking.
-  
+   //Time consuming Javascipt to calculate Primes is now in the webworker
+  // lets send a message to the webworker telling it to run that function
   calculatePrimeNumbersBtn.addEventListener('click', () => {
-    //program to print prime numbers between the two numbers
-      const lowerNumber = 0
-      const higherNumber = 400000
-      const primeNumbers = []
-
-      // looping from lowerNumber to higherNumber
-      for (let i = lowerNumber; i <= higherNumber; i++) {
-          let flag = 0;
-
-          // looping through 2 to user input number
-          for (let j = 2; j < i; j++) {
-              if (i % j == 0) {
-                  flag = 1;
-                  break;
-              }
-          }
-
-          // if number greater than 1 and not divisible by other numbers
-          if (i > 1 && flag == 0) {
-              primeNumbers.push(i);
-          }
-      }
-     console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are: `, primeNumbers);
-     results.innerHTML = primeNumbers;
+     //post message to webworker telling it what function to call
+     worker.postMessage({function: true, functionName: "calculatePrimeNumbers", message: "Hello from the main app"})
   })
+
+  //Lets listen for the message back from the webworker with the results
+  worker.onmessage = function ({ data }) {
+     console.log("We have the results back from the Web Worker")
+     results.innerHTML = data.primeNumbers
+  }
 }
 
  
---
+++ js/webworker.js
@@ -0,0 +1,38 @@
+
+//Messages from the main app are heard here
+self.onmessage = function({ data }) {
+    console.log("Imcoming message from the main app: ", data.message)
+    //check if main app is asking us to call a function. 
+    //And if so then call it by using functionName
+    if(data.function) self[data.functionName]()
+}
+
+//Now this intensive CPU work is done in a background thread and so is non-blocking.
+function calculatePrimeNumbers() {
+    //program to print prime numbers between the two numbers
+    const lowerNumber = 0
+    const higherNumber = 400000
+    const primeNumbers = []
+
+    console.log("Web worker is now calculating primes .....")
+
+    //looping from lowerNumber to higherNumber
+    for (let i = lowerNumber; i <= higherNumber; i++) {
+        let flag = 0;
+
+        // looping through 2 to user input number
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
+        }
+
+        // if number greater than 1 and not divisible by other numbers
+        if (i > 1 && flag == 0) {
+            primeNumbers.push(i);
+        }
+    }
+    //Post Message back to main app
+   self.postMessage({primeNumbers});
+}
