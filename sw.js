aed1227a20d2eff995faca6722759a9d42d72090
Zain <zain.syed@vodafone.com>
2022-11-08 17:34:23 +0000

    added to static cache

--- sw.js
+++ sw.js
@@ -3,36 +3,41 @@ const version = 1
 
 const staticCache = `staticCache_${version}`
 const staticAssets = [
-    './css/bootstrap.css',
-    'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap',
-    'https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecnFHGPezSQ.woff2',
-    './css/style.css',
-    './css/responsive.css',
-    './js/app.js',
-    './images/logo.png',
-    './images/slide-img.png',
-    './images/search-icon.png'
+  './',
+  './index.html',
+  './css/bootstrap.css',
+  'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap',
+  'https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecnFHGPezSQ.woff2',
+  'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+  './css/style.css',
+  './css/responsive.css',
+  './js/app.js',
+  './images/logo.png',
+  './images/slide-img.png',
+  './images/search-icon.png',
 ]
 
 //Listen for Installation event of Service Worker
 self.addEventListener('install', (event) => {
-    
-    console.log("Installation of Service Worker version:", version)
-    console.log(`Open cache ${staticCache} and store static assets`)
+  console.log('Installation of Service Worker version:', version)
+  console.log(`Open cache ${staticCache} and store static assets`)
 
-    event.waitUntil(
-        caches.open(staticCache)
-              .then(cache => { cache.addAll(staticAssets).then(() => console.log("assets added")) })
-              .catch((err) => console.log('Error opening cache ', err))
-    )
+  event.waitUntil(
+    caches
+      .open(staticCache)
+      .then((cache) => {
+        cache.addAll(staticAssets).then(() => console.log('assets added'))
+      })
+      .catch((err) => console.log('Error opening cache ', err))
+  )
 })
 
 //Listen for Activation event
 self.addEventListener('activate', () => {
-    console.log("Activation of Service Worker version:", version)
+  console.log('Activation of Service Worker version:', version)
 })
 
 //Listen for Fetch event
 self.addEventListener('fetch', (event) => {
-    console.log("A fetch event occured: ", event.request.url)
+  console.log('A fetch event occured: ', event.request.url)
 })
