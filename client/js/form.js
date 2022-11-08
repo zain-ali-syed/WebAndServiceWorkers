b157a9effe77fd900522021919914f1b3bb4e7f6
Zain <zain.syed@vodafone.com>
2022-11-08 17:25:35 +0000

    added console clear

--- client/js/form.js
+++ client/js/form.js
@@ -1,9 +1,8 @@
-
 function init() {
-   console.log("Initialise form functionality")
+  console.log('Initialise form functionality')
 
-   document.getElementById('sendMessageBtn')?.addEventListener("click", (ev) => {
-    ev.preventDefault();
+  document.getElementById('sendMessageBtn')?.addEventListener('click', (ev) => {
+    ev.preventDefault()
     const messageInfo = {
       id: Date.now(),
       name: document.getElementById('name').value,
@@ -19,36 +18,44 @@ function init() {
 
     //post message
     fetch('http://localhost:3000/message', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(messageInfo),
-       })
-       .then(() => console.log("Message sent to server..."))
-       .catch((err) => {
-        if(!navigator.onLine) {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(messageInfo),
+    })
+      .then(() => console.log('Message sent to server...'))
+      .catch((err) => {
+        if (!navigator.onLine) {
           //User tried sending this message when OFFLINE - Perfect to register a background sync event here
           //So when back online this message is automatically sent by the SW to the server
           ///IMAGINE SOME CODE TO ADD MESSAGE TO DB HERE.... THEN LATER WHEN ONLINE the SW will retrieve the MESSAGE FROM THE DB and send to the server
           //addtoDataBase(messageInfo)
+          console.clear()
           console.log("We are offline let's register a sync event: postMessage")
           registerBackgroundSync('postMessage')
         } else {
-          console.log("Looks like server is down rather than being offline. Background SYNC not available for this scenario - as obviously serviceWorker can't know when Server is back online")
+          console.clear()
+          console.log(
+            "Looks like server is down rather than being offline. Background SYNC not available for this scenario - as obviously serviceWorker can't know when Server is back online"
+          )
         }
 
         console.warn(err)
-    })
+      })
   })
- }
-
- //Register our sync event with our Service Worker Registration object
- function registerBackgroundSync(syncEvent) {
-    navigator.serviceWorker.ready.then((registration) => {
-      console.log(`Registering a background sync event: ${syncEvent} using our service worker registation object to call sync.register()`)
-      console.log("When we are back online this syncEvent is fired and we can hear it in the 'sync' event in our SW")
-      return registration.sync.register(syncEvent);
-    })
 }
- 
- 
- document.addEventListener("DOMContentLoaded", init)
+
+//Register our sync event with our Service Worker Registration object
+function registerBackgroundSync(syncEvent) {
+  navigator.serviceWorker.ready.then((registration) => {
+    console.clear()
+    console.log(
+      `Registering a background sync event: ${syncEvent} using our service worker registation object to call sync.register()`
+    )
+    console.log(
+      "When we are back online this syncEvent is fired and we can hear it in the 'sync' event in our SW"
+    )
+    return registration.sync.register(syncEvent)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', init)
--- client/sw.js
+++ client/sw.js
@@ -1,102 +1,104 @@
 //Service Worker Version
-const version =  2
+const version = 2
 
 const staticCache = `staticCache_${version}`
 
 const relevantCaches = [staticCache]
 
 const staticAssets = [
-    './',
-    './index.html',
-    './css/bootstrap.css',
-    'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap',
-    'https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecnFHGPezSQ.woff2',
-    'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
-    './css/style.css',
-    './css/responsive.css',
-    './js/app.js',
-    './images/logo.png',
-    './images/slide-img.png',
-    './images/search-icon.png',
-    './404.html',
-    './fallback.html',
-    './images/placeholder.jpeg'
+  './',
+  './index.html',
+  './css/bootstrap.css',
+  'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap',
+  'https://fonts.gstatic.com/s/poppins/v20/pxiEyp8kv8JHgFVrJJbecnFHGPezSQ.woff2',
+  'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+  './css/style.css',
+  './css/responsive.css',
+  './js/app.js',
+  './images/logo.png',
+  './images/slide-img.png',
+  './images/search-icon.png',
+  './404.html',
+  './fallback.html',
+  './images/placeholder.jpeg',
 ]
 
 //Listen for Installation event of Service Worker
 self.addEventListener('install', (event) => {
-    
-    console.log("Installation of Service Worker version:", version)
-    console.log(`Open cache ${staticCache} and store core static assets`)
-  
-   // The install events in service workers use waitUntil() to hold the service worker in the installing phase until tasks complete (as the service worlr is always trying to shut down to save resources). 
-   //If the promise passed to waitUntil() rejects, the install is considered a failure, and the installing service worker is discarded. 
-   //This is primarily used to ensure that a service worker is not considered installed until all of the core caches it depends on are successfully populated.
-    event.waitUntil(
-        caches.open(staticCache)
-              .then(cache => { cache.addAll(staticAssets).then(() => console.log("assets added")) })
-              .catch((err) => console.log('Error opening cache ', err))
-    )
+  console.log('Installation of Service Worker version:', version)
+  console.log(`Open cache ${staticCache} and store core static assets`)
+
+  // The install events in service workers use waitUntil() to hold the service worker in the installing phase until tasks complete (as the service worlr is always trying to shut down to save resources).
+  //If the promise passed to waitUntil() rejects, the install is considered a failure, and the installing service worker is discarded.
+  //This is primarily used to ensure that a service worker is not considered installed until all of the core caches it depends on are successfully populated.
+  event.waitUntil(
+    caches
+      .open(staticCache)
+      .then((cache) => {
+        cache.addAll(staticAssets).then(() => console.log('assets added'))
+      })
+      .catch((err) => console.log('Error opening cache ', err))
+  )
 })
 
 //Listen for Activation event
 self.addEventListener('activate', (event) => {
-    console.log("Activation of Service Worker version:", version)
-    //Now the new service worker is activated we can now delete previous
-    //caches made by the previous service worker
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                //This map returns an array of promises and when all are resolved promise.all is reslved and rturns a single
-                //promise to event.waitUntil
-                cacheNames.map(cacheName => {
-                    if (!relevantCaches.includes(cacheName)) {
-                        return caches.delete(cacheName)
-                    }
-                })
-            )
+  console.log('Activation of Service Worker version:', version)
+  //Now the new service worker is activated we can now delete previous
+  //caches made by the previous service worker
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        //This map returns an array of promises and when all are resolved promise.all is reslved and rturns a single
+        //promise to event.waitUntil
+        cacheNames.map((cacheName) => {
+          if (!relevantCaches.includes(cacheName)) {
+            return caches.delete(cacheName)
+          }
         })
-    )
+      )
+    })
+  )
 })
 
 //Listen for Fetch event
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-              .then(cacheResponse => {
-                 return cacheResponse || fetch(event.request)
-              })            
-    )
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      return cacheResponse || fetch(event.request)
+    })
+  )
 })
-
 
 //Background Sync
 //When our app comes back ONLINE and we have previously registered sync events
 //then this sync event listener is called for EACH of the registered sync events
-self.addEventListener('sync', function(event) {
-    console.log("------------BACK ONLINE SYNC EVENT-----------------")
-    console.log("Lets send the saved message to the server now")
-    if (event.tag == 'postMessage') {
-        autoPostMessageToServer()
-    }
-});
+self.addEventListener('sync', function (event) {
+  console.log('------------BACK ONLINE SYNC EVENT-----------------')
+  console.log('Lets send the saved message to the server now')
+  if (event.tag == 'postMessage') {
+    autoPostMessageToServer()
+  }
+})
 
 function autoPostMessageToServer() {
-    console.log()
-    //IMAGINE SOME CODE HERE WHICH GETS THE SAVED MESSAGE FROM THE DB AND RETURNS....
-    const messageInfo = {
-        id: 1667740139400,
-        name: 'Zain',
-        phone: '111',
-        email: 'zain@hotmail.com',
-        message: 'This is my auto sent message got from DB'
-      }
+  console.log('Post message to server')
+  //IMAGINE SOME CODE HERE WHICH GETS THE SAVED MESSAGE FROM THE DB AND RETURNS....
+  const messageInfo = {
+    id: 1667740139400,
+    name: 'Zain',
+    phone: '111',
+    email: 'zain@hotmail.com',
+    message: 'This is my auto sent message got from DB',
+  }
 
-    fetch('http://localhost:3000/message', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify(messageInfo),
-       })
-       .then(() => console.log("Message was automatically sent to server successfully"))
-       .catch(() => Promise.reject())
+  fetch('http://localhost:3000/message', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(messageInfo),
+  })
+    .then(() =>
+      console.log('Message was automatically sent to server successfully')
+    )
+    .catch(() => Promise.reject())
 }
